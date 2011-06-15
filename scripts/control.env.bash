3f7bf5886ef804366ae474c7f64094dcbd334ca3
Ciprian Dorin, Craciun <ciprian@volution.ro>
2011-06-15 19:16:06 +0300

    --

---
+++ scripts/control.env.bash
@@ -0,0 +1,40 @@
+#!/dev/null
+
+_erl_path=''
+
+_erl_run_argv=(
+	+Bd +Ww
+	-env ERL_CRASH_DUMP /dev/null
+	-env ERL_LIBS "${_deployment_erlang_path:-./erlang}/lib"
+	-env LANG C
+	-noshell -noinput
+	-config "${_deployment_erlang_path:-./erlang}/lib/mosaic_riak_kv/priv/mosaic_riak_kv.config"
+	-run mosaic_riak_kv_callbacks standalone
+)
+
+_ez_bundle_names=(
+	basho_stats
+	bitcask
+	cluster_info
+	ebloom
+	eper
+	erlang_js
+	luke
+	luwak
+	mochiweb
+	mosaic_component
+	mosaic_harness
+	mosaic_riak_kv
+	mosaic_tools
+	protobuffs
+	riakc
+	riak_core
+	riak_err
+	riak_kv
+	skerl
+	webmachine
+)
+
+_bundles_token="436d2c5d9b46d5ae336eabf38dc920a3"
+_bundles_base_url="http://data.volution.ro/ciprian/${_bundles_token}"
+_bundles_base_path="/afs/olympus.volution.ro/people/ciprian/web/data/${_bundles_token}"
