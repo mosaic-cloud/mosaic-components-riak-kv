9a610da68f3fcdf1fe09917570a9243b3d03edbf
Ciprian Dorin Craciun <ciprian@volution.ro>
2014-07-04 14:51:17 +0300

    Fix garbage-less build

--- applications/erlang-js/generate.bash
+++ applications/erlang-js/generate.bash
@@ -8,21 +8,27 @@ test "${#}" -eq 0
 cd -- "$( dirname -- "$( readlink -e -- "${0}" )" )"
 test -d "${_generate_outputs}"
 
-make -C ./repositories/erlang-js/c_src clean
-make -C ./repositories/erlang-js/c_src js
+mkdir "${_generate_outputs}/build"
+
+( cd -- ./repositories/erlang-js ; exec find . -not -name '.git' -print0 ; ) \
+| ( cd -- ./repositories/erlang-js ; exec cpio -p -0 --quiet -- "${_generate_outputs}/build" ; )
+chmod -R a=rX,u=rwX -- "${_generate_outputs}/build"
+
+make -C "${_generate_outputs}/build/c_src" clean
+make -C "${_generate_outputs}/build/c_src" js
 
 gcc -shared -o "${_generate_outputs}/erlang_js_drv.so" \
-		-L ./repositories/erlang-js/c_src/system/lib \
-		-I ./repositories/erlang-js/c_src/system/include/js \
-		-I ./repositories/erlang-js/c_src/system/include/nspr \
-		-I ./repositories/erlang-js/c_src \
+		-L "${_generate_outputs}/build/c_src/system/lib" \
+		-I "${_generate_outputs}/build/c_src/system/include/js" \
+		-I "${_generate_outputs}/build/c_src/system/include/nspr" \
+		-I "${_generate_outputs}/build/c_src" \
 		-I "${pallur_pkg_erlang:-/usr/lib/erlang}/usr/include" \
 		-L "${pallur_pkg_erlang:-/usr/lib/erlang}/usr/lib" \
 		-w \
 		${pallur_CFLAGS:-} ${pallur_LDFLAGS:-} \
 		-DXP_UNIX \
-		./repositories/erlang-js/c_src/{driver_comm.c,spidermonkey.c,spidermonkey_drv.c} \
-		./repositories/erlang-js/c_src/system/lib/{libjs.a,libnspr4.a} \
+		"${_generate_outputs}/build/c_src/"{driver_comm.c,spidermonkey.c,spidermonkey_drv.c} \
+		"${_generate_outputs}/build/c_src/system/lib/"{libjs.a,libnspr4.a} \
 		${pallur_LIBS:-}
 
 exit 0
